55f765fb8909d362e1940fc076d0c5473fbf26d3
skystoneLocal <adityaspmahajan@gmail.com>
2021-01-22 22:55:17 +0530

    Lokavidya Sub channel Api test

---
+++ Retrofit Sub channel test/app/src/main/java/com/example/retrofit/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+        // http://lvcms.lokavidya.com//api/v1/channels/details?channel_id=180&user_id=530&org_user_id=406&is_active=true&page=1
+        Retrofit retrofit = new Retrofit.Builder()
+                //.baseUrl("http://jsonplaceholder.typicode.com/")
+                .baseUrl("http://lvcms.lokavidya.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        // getPosts();
+
+        // getComments();
+
+        createPost();
+    }
+
+    private void getPosts(){
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(5,"id", "desc");
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                for(Post post : posts){
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments(){
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(4);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+ response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment : comments){
+                    String content = "";
+                    content += "ID: " + comment.getId() + "\n";
+                    content += "Post ID: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName() + "\n";
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Text: " + comment.getText() + "\n";
+                    content += "\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void createPost(){
+        // http://lvcms.lokavidya.com//api/v1/channels/details?channel_id=180&user_id=530&org_user_id=406&is_active=true&page=1
+
+        // Way 1
+        // Post post = new Post(23,"New Title","new text");
+        // Call<Post> call = jsonPlaceHolderApi.createPost(post);
+
+        // Way 2
+        // Call<Post> call = jsonPlaceHolderApi.createPost(23,"New Title","new text");
+
+        // Way 3
+        Map<String, String> fields = new HashMap<>();
+        fields.put("userId", " 25");
+        fields.put("title","new Title");
+        Call<Post> call = jsonPlaceHolderApi.createPost(fields);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n";
+
+                textViewResult.append(content);
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
---
+++ Retrofit Sub channel test/app/src/main/java/com/example/retrofit/model/ChannelModel.java
@@ -0,0 +1,155 @@
+package com.example.retrofit.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "description",
+        "created_by",
+        "timestamp",
+        "subchannel_count",
+        "videos_count",
+        "parent_channel",
+        "Videos"
+})
+
+public class ChannelModel {
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("created_by")
+    private Object createdBy;
+    @JsonProperty("timestamp")
+    private String timestamp;
+    @JsonProperty("subchannel_count")
+    private Integer subchannelCount;
+    @JsonProperty("videos_count")
+    private Integer videosCount;
+    @JsonProperty("parent_channel")
+    private String parentChannel;
+    @JsonProperty("Videos")
+    private Object videos;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonProperty("description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @JsonProperty("created_by")
+    public Object getCreatedBy() {
+        return createdBy;
+    }
+
+    @JsonProperty("created_by")
+    public void setCreatedBy(Object createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @JsonProperty("timestamp")
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    @JsonProperty("timestamp")
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @JsonProperty("subchannel_count")
+    public Integer getSubchannelCount() {
+        return subchannelCount;
+    }
+
+    @JsonProperty("subchannel_count")
+    public void setSubchannelCount(Integer subchannelCount) {
+        this.subchannelCount = subchannelCount;
+    }
+
+    @JsonProperty("videos_count")
+    public Integer getVideosCount() {
+        return videosCount;
+    }
+
+    @JsonProperty("videos_count")
+    public void setVideosCount(Integer videosCount) {
+        this.videosCount = videosCount;
+    }
+
+    @JsonProperty("parent_channel")
+    public String getParentChannel() {
+        return parentChannel;
+    }
+
+    @JsonProperty("parent_channel")
+    public void setParentChannel(String parentChannel) {
+        this.parentChannel = parentChannel;
+    }
+
+    @JsonProperty("Videos")
+    public Object getVideos() {
+        return videos;
+    }
+
+    @JsonProperty("Videos")
+    public void setVideos(Object videos) {
+        this.videos = videos;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+//    @Override
+//    public String toString() {
+//        return new ToStringBuilder(this).append("id", id).append("name", name).append("description", description).append("createdBy", createdBy).append("timestamp", timestamp).append("subchannelCount", subchannelCount).append("videosCount", videosCount).append("parentChannel", parentChannel).append("videos", videos).append("additionalProperties", additionalProperties).toString();
+//    }
+
+}
+
+
