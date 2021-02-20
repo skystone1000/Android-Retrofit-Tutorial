a8f5e9a83fc7d6528e06c153faae7ca778704e66
skystoneLocal <adityaspmahajan@gmail.com>
2021-02-21 00:52:28 +0530

    Retorofit Tutorial 2

--- Retrofit Tutorial 2/app/src/main/java/com/example/retrofit/JsonPlaceHolderApi.java
+++ Retrofit Tutorial 2/app/src/main/java/com/example/retrofit/JsonPlaceHolderApi.java
@@ -4,9 +4,18 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface JsonPlaceHolderApi {
 
     @GET("posts")
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(
+            @Query("userId") int userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
 }
--- Retrofit Tutorial 2/app/src/main/java/com/example/retrofit/MainActivity.java
+++ Retrofit Tutorial 2/app/src/main/java/com/example/retrofit/MainActivity.java
@@ -16,6 +16,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
 
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+         getPosts();
+
+//        getComments();
+    }
+
+    private void getPosts(){
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(5,"id", "desc");
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -55,6 +63,40 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments(){
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(4);
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+ response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment : comments){
+                    String content = "";
+                    content += "ID: " + comment.getId() + "\n";
+                    content += "Post ID: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName() + "\n";
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Text: " + comment.getText() + "\n";
+                    content += "\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
